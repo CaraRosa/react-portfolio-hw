bad607551a8624b839e4af3df1f680517e136370
Cara R <cara.rosa.02@gmail.com>
2024-01-26 10:06:23 -0600

    react and vite setup and about me page working

---
+++ react-portfolio/src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import AboutMe from './pages/AboutMe';
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />
+      }
+    ],
+  },
+]);
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
