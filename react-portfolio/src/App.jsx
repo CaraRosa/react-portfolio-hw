f46666b818de792d045f91f39cb156ca1ee7964c
Cara R <cara.rosa.02@gmail.com>
2024-01-28 11:54:50 -0600

    added header and navigation component but need to style them

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,15 +1,45 @@
 import { Outlet } from 'react-router-dom';
-import Nav from './components/NavTabs';
+// import Nav from './components/NavTabs';
+import Navigation from './components/Navigation';
 import Footer from './components/footer';
+import Header from './components/Header';
+import React, { useState } from "react";
+import AboutMe from './pages/AboutMe';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css';
 
 function App() {
+  const [pages] = useState([
+    {
+      name: "about me",
+    },
+    {
+      name: "contact"
+    },
+    {
+      name: 'portfolio'
+    },
+    {
+      name: 'resume',
+    }
+  ]);
+
+  const [currentPage, setCurrentPage] = useState(pages[0]);
   return (
     <>
-    < Nav />
+    < Navigation
+      className="App-navigation"
+      setCurrentPage = {setCurrentPage}
+      currentPage={currentPage}
+      pages={pages}
+      />
+
+    <Header />
     <main className='mx-3'>
       <Outlet />
     </main> 
--- react-portfolio/src/components/header.jsx
+++ react-portfolio/src/components/header.jsx
@@ -4,22 +4,9 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">About Me</Link>
-          </li>
-          <li>
-            <Link to="/portfolio">Portfolio</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/resume">Resume</Link>
-          </li>
-        </ul>
-      </nav>
+      <div>
+      Welcome to my portfolio!
+      </div>
     </header>
   );
 };
